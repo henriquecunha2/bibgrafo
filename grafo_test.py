46aaf02d74032d7279249fadfc79bf50f41a80ab
henrique <henrique.cunha@ifpb.edu.br>
2021-02-27 21:13:14 -0300

    Consertando bug no método GrafoLista.existeAresta() que chamava GrafoLista.arestaValida() sem necessidade.

--- grafo_test.py
+++ grafo_test.py
@@ -1,12 +1,12 @@
 import unittest
-from meu_grafo import *
+from bibgrafo.grafo_lista_adjacencia import *
 from bibgrafo.grafo_exceptions import *
 
 class TestGrafo(unittest.TestCase):
 
     def setUp(self):
         # Grafo da Paraíba
-        self.g_p = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p = GrafoListaAdjacencia(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
         self.g_p.adicionaAresta('a1', 'J', 'C')
         self.g_p.adicionaAresta('a2', 'C', 'E')
         self.g_p.adicionaAresta('a3', 'C', 'E')
@@ -19,7 +19,7 @@ class TestGrafo(unittest.TestCase):
 
 
         # Grafo da Paraíba sem arestas paralelas
-        self.g_p_sem_paralelas = MeuGrafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        self.g_p_sem_paralelas = GrafoListaAdjacencia(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
         self.g_p_sem_paralelas.adicionaAresta('a1', 'J', 'C')
         self.g_p_sem_paralelas.adicionaAresta('a2', 'C', 'E')
         self.g_p_sem_paralelas.adicionaAresta('a3', 'P', 'C')
@@ -29,7 +29,7 @@ class TestGrafo(unittest.TestCase):
         self.g_p_sem_paralelas.adicionaAresta('a7', 'T', 'Z')
 
         # Grafos completos
-        self.g_c = MeuGrafo(['J', 'C', 'E', 'P'])
+        self.g_c = GrafoListaAdjacencia(['J', 'C', 'E', 'P'])
         self.g_c.adicionaAresta('a1','J','C')
         self.g_c.adicionaAresta('a2', 'J', 'E')
         self.g_c.adicionaAresta('a3', 'J', 'P')
@@ -37,37 +37,37 @@ class TestGrafo(unittest.TestCase):
         self.g_c.adicionaAresta('a5', 'P', 'C')
         self.g_c.adicionaAresta('a6', 'P', 'E')
 
-        self.g_c2 = MeuGrafo(['Nina', 'Maria'])
+        self.g_c2 = GrafoListaAdjacencia(['Nina', 'Maria'])
         self.g_c2.adicionaAresta('amiga', 'Nina', 'Maria')
 
-        self.g_c3 = MeuGrafo(['J'])
+        self.g_c3 = GrafoListaAdjacencia(['J'])
 
         # Grafos com laco
-        self.g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l1 = GrafoListaAdjacencia(['A', 'B', 'C', 'D'])
         self.g_l1.adicionaAresta('a1', 'A', 'A')
         self.g_l1.adicionaAresta('a2', 'A', 'B')
         self.g_l1.adicionaAresta('a3', 'A', 'A')
 
-        self.g_l2 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l2 = GrafoListaAdjacencia(['A', 'B', 'C', 'D'])
         self.g_l2.adicionaAresta('a1', 'A', 'B')
         self.g_l2.adicionaAresta('a2', 'B', 'B')
         self.g_l2.adicionaAresta('a3', 'B', 'A')
 
-        self.g_l3 = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_l3 = GrafoListaAdjacencia(['A', 'B', 'C', 'D'])
         self.g_l3.adicionaAresta('a1', 'C', 'A')
         self.g_l3.adicionaAresta('a2', 'C', 'C')
         self.g_l3.adicionaAresta('a3', 'D', 'D')
         self.g_l3.adicionaAresta('a4', 'D', 'D')
 
-        self.g_l4 = MeuGrafo(['D'])
+        self.g_l4 = GrafoListaAdjacencia(['D'])
         self.g_l4.adicionaAresta('a1', 'D', 'D')
 
-        self.g_l5 = MeuGrafo(['C', 'D'])
+        self.g_l5 = GrafoListaAdjacencia(['C', 'D'])
         self.g_l5.adicionaAresta('a1', 'D', 'C')
         self.g_l5.adicionaAresta('a2', 'C', 'C')
 
         # Grafos desconexos
-        self.g_d = MeuGrafo(['A', 'B', 'C', 'D'])
+        self.g_d = GrafoListaAdjacencia(['A', 'B', 'C', 'D'])
         self.g_d.adicionaAresta('asd', 'A', 'B')
 
     def test_adiciona_aresta(self):
@@ -84,6 +84,21 @@ class TestGrafo(unittest.TestCase):
             self.g_p.adicionaAresta('x', 'J', 'V')
         with self.assertRaises(ArestaInvalidaException):
             self.g_p.adicionaAresta('a1', 'J', 'C')
+
+    def test_eq(self):
+        g_p_eq = GrafoListaAdjacencia()
+        g_p_eq = GrafoListaAdjacencia(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
+        g_p_eq.adicionaAresta('a1', 'J', 'C')
+        g_p_eq.adicionaAresta('a2', 'C', 'E')
+        g_p_eq.adicionaAresta('a3', 'C', 'E')
+        g_p_eq.adicionaAresta('a4', 'P', 'C')
+        g_p_eq.adicionaAresta('a5', 'P', 'C')
+        g_p_eq.adicionaAresta('a6', 'T', 'C')
+        g_p_eq.adicionaAresta('a7', 'M', 'C')
+        g_p_eq.adicionaAresta('a8', 'M', 'T')
+        g_p_eq.adicionaAresta('a9', 'T', 'Z')
+
+        self.assertEqual(self.g_p, g_p_eq)
 
     def test_vertices_nao_adjacentes(self):
         self.assertEqual(self.g_p.vertices_nao_adjacentes(), ['J-E', 'J-P', 'J-M', 'J-T', 'J-Z', 'C-Z', 'E-J', 'E-P', 'E-M', 'E-T', 'E-Z', 'P-J', 'P-E', 'P-M', 'P-T', 'P-Z', 'M-J', 'M-E', 'M-P', 'M-Z', 'T-J', 'T-E', 'T-P', 'Z-J', 'Z-C', 'Z-E', 'Z-P', 'Z-M'])
