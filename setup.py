27c878d601d59abfabc78e76beb2d9ffffa6d4aa
henrique <henrique.cunha@ifpb.edu.br>
2021-07-14 11:57:33 -0300

    Adiocionando o setup da versão 0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='bibgrafo',
     packages=find_packages(include=['bibgrafo']),
-    version='0.8',
+    version='0.9',
     description='Bibliteca de grafos para ensino de Teoria dos Grafos',
     author='Henrique do Nascimento Cunha',
     license='MIT',
