9714a8b611fd197f88f92deebfb8b2d1e0a4d1d9
henrique <henrique.cunha@ifpb.edu.br>
2021-09-17 09:30:54 -0300

    Grafo direcionado com matriz de adjacência não direcionado agora contempla grafos ponderados.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='bibgrafo',
     packages=find_packages(include=['bibgrafo']),
-    version='0.9.2',
+    version='0.9.4',
     description='Biblioteca para ensino de Teoria dos Grafos',
     author='Henrique do Nascimento Cunha',
     license='MIT',
