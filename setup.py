b61060dc0df781db229e2a7929e03272f7b8397d
Henrique Cunha <henrique.cunha@ifpb.edu.br>
2022-09-06 19:57:14 -0300

    Corrigido bug do __str__ da matriz de adjacência não direcionado

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
 setup(
     name='bibgrafo',
-    packages=find_packages(include=['bibgrafo', 'multipledispatch']),
-    version='0.9.5',
+    packages=find_packages(include=['bibgrafo']),
+    version='1.0.1',
     description='Biblioteca para ensino de Teoria dos Grafos',
     author='Henrique do Nascimento Cunha',
+    install_requires=[
+          'multipledispatch',
+    ],
     license='MIT',
 )
