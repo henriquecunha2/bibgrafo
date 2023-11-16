4bdd377ae0168fc0868d38099df9bfaafd91db67
SymonBezerra <simaobezerratranslate@gmail.com>
2023-11-16 20:42:57 -0300

    Ajustes finais + code review

--- bibgrafo/grafo_builder.py
+++ bibgrafo/grafo_builder.py
@@ -1,4 +1,4 @@
-from random import randrange, shuffle
+from random import randrange, shuffle, randint
 from bibgrafo.grafo_errors import GrafoBuilderError, GrafoInvalidoError
 from bibgrafo.grafo import GrafoIF
 
@@ -7,8 +7,6 @@ class GrafoBuilder:
         self.__grafo = None
 
     def build(self):
-        # TODO: O build deve gravar o grafo construído em um arquivo JSON
-        # (construir um bibgrafo.grafo_reader)
         return self.__grafo
 
     def tipo(self, grafo: GrafoIF):
@@ -19,13 +17,25 @@ class GrafoBuilder:
         return self
 
     def vertices(self, qtd: int=1, start: chr='A', vertices=[]):
+        if not start.isupper() or not start.isalpha():
+            raise GrafoBuilderError('O rótulo dos vértices deve ser uma letra maiúscula')
         if vertices:
             for v in vertices:
                 self.__grafo.adiciona_vertice(v)
         else:
+            MAX = ord('Z') - ord('A')
+            START_MAX = ord('Z') - ord(start) + 1
             for i in range(qtd):
-                self.__grafo.adiciona_vertice(chr(ord(start) + i))
-        # TODO: e se qtd > 26? (alfabeto) gerar 27 == 'AA'
+                if i >= START_MAX:
+                    rotulo_v = []
+                    n = i
+                    while n >= START_MAX:
+                        m = n % START_MAX
+                        rotulo_v.insert(0, chr(ord('A') + m))
+                        n //= START_MAX
+                    rotulo_v.insert(0, chr(ord('A') + n - 1))
+                    self.__grafo.adiciona_vertice(''.join(rotulo_v))
+                else: self.__grafo.adiciona_vertice(chr(ord(start) + i))
         return self
 
     def arestas(self, arestas=list(), qtd: int=1, completo=False, laco=False, qtd_laco=1,
@@ -33,7 +43,7 @@ class GrafoBuilder:
 
         def peso(peso_max: int):
             if peso_max <= peso_min: raise GrafoBuilderError('O intervalo para o peso das arestas é inválido')
-            elif peso_max == 1 and peso_min: return 1
+            elif peso_max == 1 and peso_min == 0: return 1
             else: return randrange(peso_min, peso_max)
 
         if len(arestas) > 0:
@@ -56,7 +66,10 @@ class GrafoBuilder:
                 name_a = chr(ord(name_a) + 1)
 
         else:
-            adjacentes = set()
+            pares_adjacentes = list()
+            for v1 in range(len(vertices)):
+                for v2 in range(v1 + 1, len(vertices)):
+                    pares_adjacentes.append([vertices[v1], vertices[v2]])
             if not conexo:
                 if qtd_desconexo > len(vertices):
                     raise GrafoBuilderError('Não é possível gerar um grafo desconexo com configurações passadas')
@@ -79,23 +92,17 @@ class GrafoBuilder:
                     self.__grafo.adiciona_aresta(str(i) + 'l',
                         vertices[v], vertices[v], peso(peso_max))
 
-            if qtd > (len(vertices) * (len(vertices) - 1)) / 2: # créditos ao ChatGPT
+            if qtd > (len(vertices) * (len(vertices) - 1)) / 2:
                 raise GrafoBuilderError('Não é possível gerar um grafo com esta quantidade de arestas')
             for i in range(qtd):
-                # TODO: gerar uma lista de todos os pares possíveis, ao invés de usar um while
+                par_v: list
                 if len(vertices) > 2:
-                    while True:
-                        v1 = randrange(len(vertices))
-                        v2 = randrange(len(vertices))
-                        par_adj = '%s-%s' % (vertices[v1], vertices[v2])
-                        if par_adj not in adjacentes and v2 != v1: break
+                    par_v = pares_adjacentes.pop(randint(0, len(pares_adjacentes) - 1))
                 else:
-                    v1, v2 = 0, 1
-                par_v = [vertices[v1], vertices[v2]]
+                    par_v = pares_adjacentes.pop()
                 shuffle(par_v)
                 self.__grafo.adiciona_aresta(name_a + str(i),
                     par_v[0], par_v[1], peso(peso_max))
-                adjacentes.add('%s-%s' % (vertices[v1], vertices[v2]))
 
             if paralelas:
                 arestas = [a for a in self.__grafo.arestas.values()]
--- grafo_builder_test.py
+++ grafo_builder_test.py
@@ -5,12 +5,13 @@ from meu_grafo_teste_builder import *
 
 class TestBuilderGrafo(unittest.TestCase):
 
-    def test_arestas(self):
-        pass
+    def test_rotulo_vertices(self):
+        for i in range(50):
+            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i).build()
+            for v in grafo.vertices: self.assertTrue(v.rotulo.isalpha() and v.rotulo.isupper())
 
     def test_grafo_completo(self):
-        for i in range(50):
-            if i == 0: continue
+        for i in range(1, 30):
             grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i)\
                 .arestas(completo=True).build()
             self.assertTrue(grafo.eh_completo())
