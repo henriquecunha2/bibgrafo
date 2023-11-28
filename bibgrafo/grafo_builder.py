2ca8b061fdcbcb62068226f5c60c14ed0790302b
SymonBezerra <simaobezerratranslate@gmail.com>
2023-11-28 12:45:12 -0300

    Singledispatch para GrafoBuilder.vertices() impl

--- bibgrafo/grafo_builder.py
+++ bibgrafo/grafo_builder.py
@@ -38,7 +38,8 @@ class GrafoBuilder:
         self._grafo = grafo
         return self
 
-    def vertices(self, qtd: int=1, start: chr='A', vertices=[]):
+    @singledispatchmethod
+    def vertices(self, arg:int, **kwargs):
         '''
             Recebe os parâmetros para inserção dos vértices pelo GrafoBuilder.
             Args:
@@ -52,28 +53,30 @@ class GrafoBuilder:
                 GrafoBuilderError: caso a lista de vértices não contenha objetos do tipo bibgrafo.Vertice
                 ou caso o caractere passado para 'start' não seja uma letra maiúscula
         '''
-        if vertices:
-            for v in vertices:
-                try:
-                    self._grafo.adiciona_vertice(v)
-                except NotImplementedError:
-                    raise GrafoBuilderError('A lista de vértices deve conter objetos do tipo Vertice')
-        else:
-            if not start.isupper() or not start.isalpha():
-                raise GrafoBuilderError('O rótulo dos vértices deve ser uma letra maiúscula')
+        start = kwargs.get('start', 'A')
+        if not start.isupper() or not start.isalpha():
+            raise GrafoBuilderError('O rótulo dos vértices deve ser uma letra maiúscula')
 
-            START_MAX = ord('Z') - ord(start) + 1
-            for i in range(qtd):
-                if i >= START_MAX:
-                    rotulo_v = []
-                    n = i
-                    while n >= START_MAX:
-                        m = n % START_MAX
-                        rotulo_v.insert(0, chr(ord('A') + m))
-                        n //= START_MAX
-                    rotulo_v.insert(0, chr(ord('A') + n - 1))
-                    self._grafo.adiciona_vertice(''.join(rotulo_v))
-                else: self._grafo.adiciona_vertice(chr(ord(start) + i))
+        START_MAX = ord('Z') - ord(start) + 1
+        for i in range(arg):
+            if i >= START_MAX:
+                rotulo_v = []
+                n = i
+                while n >= START_MAX:
+                    m = n % START_MAX
+                    rotulo_v.insert(0, chr(ord('A') + m))
+                    n //= START_MAX
+                rotulo_v.insert(0, chr(ord('A') + n - 1))
+                self._grafo.adiciona_vertice(''.join(rotulo_v))
+            else: self._grafo.adiciona_vertice(chr(ord(start) + i))
+        return self
+
+    @vertices.register
+    def _(self, arg: list, **kwargs):
+        if len(arg) == 0: raise GrafoBuilderError('A lista de vértices não pode estar vazia')
+        for v in arg:
+            self._grafo.adiciona_vertice(v)
+
         return self
 
     def _peso(self, min, max):
