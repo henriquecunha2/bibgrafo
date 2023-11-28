bd80c39763581e315c1c18ca8014cb3c5f2b0ec1
SymonBezerra <simaobezerratranslate@gmail.com>
2023-11-28 12:48:39 -0300

    Testes unitários GrafoBuilder revisados

--- grafo_builder_test.py
+++ grafo_builder_test.py
@@ -7,33 +7,33 @@ class TestBuilderGrafo(unittest.TestCase):
 
     def test_rotulo_vertices(self):
         for i in range(50):
-            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i).build()
+            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(i).build()
             for v in grafo.vertices: self.assertTrue(v.rotulo.isalpha() and v.rotulo.isupper())
 
     def test_grafo_completo(self):
         for i in range(1, 30):
-            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i)\
-                .arestas(completo=True).build()
+            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(i)\
+                .arestas(True).build()
             self.assertTrue(grafo.eh_completo())
 
     def test_grafo_simples(self):
         for i in range(2, 20):
             for j in range(i, 100):
                 try:
-                    grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i) \
-                        .arestas(qtd=j).build()
+                    grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(i) \
+                        .arestas(j).build()
                     self.assertFalse(grafo.ha_laco() and grafo.ha_paralelas())
                 except GrafoBuilderError: continue
 
     def test_laco_paralelas(self):
         for i in range(2, 20):
             for j in range(1, i):
-                grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i) \
-                    .arestas(qtd=i - 1, paralelas=True, qtd_paralelas=j, laco=True, qtd_laco=j).build()
+                grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(i) \
+                    .arestas(i - 1, paralelas=1, lacos=1).build()
                 self.assertTrue(grafo.ha_paralelas() and grafo.ha_laco())
 
     def test_grafo_conexo(self):
         for i in range(1, 20):
-            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(qtd=i) \
-                .arestas(completo=True).build()
+            grafo = GrafoBuilder().tipo(MeuGrafo()).vertices(i) \
+                .arestas(True).build()
             self.assertTrue(grafo.conexo())
