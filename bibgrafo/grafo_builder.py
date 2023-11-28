b2defe9aa940f2b5bebea69b2c6f8f44ff34bc45
SymonBezerra <simaobezerratranslate@gmail.com>
2023-11-28 12:36:29 -0300

    Singledispatch para GrafoBuilder.arestas() impl

--- bibgrafo/grafo_builder.py
+++ bibgrafo/grafo_builder.py
@@ -1,3 +1,4 @@
+from functools import singledispatchmethod
 from random import randrange, shuffle, randint
 from bibgrafo.grafo_errors import GrafoBuilderError, GrafoInvalidoError
 from bibgrafo.grafo import GrafoIF
@@ -10,18 +11,18 @@ class GrafoBuilder:
         aleatórios ou pode receber vértices e arestas já criados pelo usuário.
 
         Attributes:
-            __grafo: receberá a instância de GrafoIF na qual serão realizadas as operações
+            _grafo: receberá a instância de GrafoIF na qual serão realizadas as operações
             de inserção de vértices e arestas, conforme requerido.
     '''
     def __init__(self):
-        self.__grafo = None
+        self._grafo = None
 
     def build(self):
         '''
             Esta função deve ser chamada ao final de todas as outras, retornando a instância
             do grafo criado pelo GrafoBuilder.
         '''
-        return self.__grafo
+        return self._grafo
 
     def tipo(self, grafo: GrafoIF):
         '''
@@ -34,7 +35,7 @@ class GrafoBuilder:
         if not isinstance(grafo, GrafoIF):
             raise GrafoInvalidoError('A classe utilizada não é um grafo')
 
-        self.__grafo = grafo
+        self._grafo = grafo
         return self
 
     def vertices(self, qtd: int=1, start: chr='A', vertices=[]):
@@ -54,7 +55,7 @@ class GrafoBuilder:
         if vertices:
             for v in vertices:
                 try:
-                    self.__grafo.adiciona_vertice(v)
+                    self._grafo.adiciona_vertice(v)
                 except NotImplementedError:
                     raise GrafoBuilderError('A lista de vértices deve conter objetos do tipo Vertice')
         else:
@@ -71,113 +72,91 @@ class GrafoBuilder:
                         rotulo_v.insert(0, chr(ord('A') + m))
                         n //= START_MAX
                     rotulo_v.insert(0, chr(ord('A') + n - 1))
-                    self.__grafo.adiciona_vertice(''.join(rotulo_v))
-                else: self.__grafo.adiciona_vertice(chr(ord(start) + i))
+                    self._grafo.adiciona_vertice(''.join(rotulo_v))
+                else: self._grafo.adiciona_vertice(chr(ord(start) + i))
         return self
 
-    def arestas(self, arestas=list(), completo=False, qtd=1, laco=False, qtd_laco=1,
-        conexo=True, qtd_desconexo=2, paralelas=False, qtd_paralelas=1, peso_max=1, peso_min=0):
-        '''
-            Esta função recebe os parâmetros para inserção das arestas pelo GrafoBuilder.
-            Args:
-                arestas: recebe uma lista de objetos do tipo bibgrafo.Aresta ou bibgrafo.ArestaDirecionada,
-                para serem inseridas pelo GrafoBuilder. Caso essa opção seja utilizada, todas as outras serão
-                ignoradas.
-                completo: se marcada como True, o grafo gerado será obrigatoriamente um grafo completo
-                a partir dos vértices gerados na função anterior. Caso essa opção seja utilizada,
-                todas as outras a partir dela serão ignoradas, tendo ela prioridade logo após a opção 'arestas'.
+    def _peso(self, min, max):
+        if min >= max: raise GrafoBuilderError('Forneça um intervalo válido para o peso das arestas')
+        if min == 0 and max == 1: return max
+        else: return randint(min, max)
 
-                Caso nenhuma dessas opções seja utilizada, o GrafoBuilder irá gerar um grafo simples.
-                qtd: a quantidade de arestas a serem adicionadas.
-                laco: caso marcada, após a criação de arestas para um grafo simples, a função irá
-                adicionar laços no grafo.
-                qtd_laco: a quantidade de lacos a serem adicionados.
-                conexo: se marcada como False, o GrafoBuilder irá gerar um grafo completo com alguns
-                vértices desconexos e adicionar as suas arestas ao seu próprio grafo.
-                qtd_desconexo: a quantidade de vértices desconexos.
-                paralelas: se marcada como True, o GrafoBuilder irá gerar arestas paralelas após a criação
-                de um grafo simples.
-                qtd_paralelas: a quantidade de arestas paralelas presentes no grafo.
+    @singledispatchmethod
+    def arestas(self, arg: int, **kwargs):
+        vertices = [v.rotulo for v in self._grafo.vertices]
+        if arg > (len(vertices) * (len(vertices) - 1)) / 2:
+            raise GrafoBuilderError('Não é possível construir um grafo com esta quantidade de arestas')
 
-                (As opções a seguir também podem ser utilizadas na criação de grafos completos)
-                peso_max: o peso máximo das arestas.
-                peso_min: o peso mínimo das arestas.
-            Raises:
-                GrafoBuilderError:
-                    - quando o intervalo de peso_max e peso_min é inválido
-                    - quando 'qtd' excede [n + (n - 1)] / 2, sendo 'n' o número de vértices
-                    - quando a quantidade de vértices desconexos é maior que o número de vértices
-                    - se a lista de vértices estiver vazia
-        '''
+        desconexos = kwargs.get('desconexos', 0)
+        if desconexos < 0 or desconexos > len(vertices):
+            raise GrafoBuilderError('Forneça um número válido para os vértices desconexos')
+        lacos = kwargs.get('lacos', 0)
+        if lacos < 0: raise GrafoBuilderError('Forneça um número de laços positivo')
+        paralelas = kwargs.get('paralelas', 0)
+        if paralelas < 0: raise GrafoBuilderError('Forneça um número de arestas paralelas positivo')
+        peso_min = kwargs.get('peso_min', 0)
+        peso_max = kwargs.get('peso_max', 1)
 
-        def peso(peso_max: int):
-            if peso_max <= peso_min: raise GrafoBuilderError('O intervalo para o peso das arestas é inválido')
-            elif peso_max == 1 and peso_min == 0: return 1
-            else: return randrange(peso_min, peso_max)
+        vertices_desc = []
+        if desconexos:
+            for i in range(desconexos):
+                v_desc = vertices.pop(randint(0, len(vertices) - 1))
+                vertices_desc.append(v_desc)
+            rotulo_v = 1
+            if len(vertices_desc) > 1:
+                for v1 in range(len(vertices_desc)):
+                    for v2 in range(v1 + 1, len(vertices_desc)):
+                        self._grafo.adiciona_aresta('{}c'.format(rotulo_v),
+                        vertices_desc[v1], vertices_desc[v2], self._peso(peso_min, peso_max))
+                        rotulo_v += 1
+            else:
+                self._grafo.adiciona_aresta('{}c'.format(rotulo_v),
+                    vertices_desc[0], vertices_desc[0], self._peso(peso_min, peso_max))
 
-        if len(arestas) > 0:
-            for a in arestas:
-                try: self.__grafo.adiciona_aresta(a)
-                except NotImplementedError: raise GrafoBuilderError('Este grafo não suporta este tipo de Aresta')
-            return self
+        pares = []
+        adjacentes = []
 
-        name_a = 'a'
-        vertices = [v.rotulo for v in self.__grafo.vertices]
-        if completo:
-            if len(vertices) == 0:
-                raise GrafoBuilderError('A lista de vértices ainda está vazia.')
-            for v1 in range(len(vertices)):
-                for v2 in range(v1 + 1, len(vertices)):
-                    par_v = [vertices[v1], vertices[v2]]
-                    shuffle(par_v)
-                    self.__grafo.adiciona_aresta(name_a + str(v2),
-                        par_v[0], par_v[1], peso(peso_max))
-                name_a = chr(ord(name_a) + 1)
+        for v1 in range(len(vertices)):
+            for v2 in range(v1 + 1, len(vertices)):
+                pares.append([vertices[v1], vertices[v2]])
 
-        else:
-            pares_adjacentes = list()
-            pares_arestas = list()
-            for v1 in range(len(vertices)):
-                for v2 in range(v1 + 1, len(vertices)):
-                    pares_adjacentes.append([vertices[v1], vertices[v2]])
-            if not conexo:
-                if qtd_desconexo > len(vertices):
-                    raise GrafoBuilderError('Não é possível gerar um grafo desconexo com configurações passadas')
+        for i in range(arg):
+            par = pares.pop(randint(0, len(pares) - 1)) if len(pares) > 1 else pares[0]
+            shuffle(par)
+            adjacentes.append(par)
+            self._grafo.adiciona_aresta('a{}'.format(i), par[0],
+                par[1], self._peso(peso_min, peso_max))
 
-                vertices_desconexos = []
-                for i in range(qtd_desconexo):
-                    v = vertices.pop(randrange(len(vertices)))
-                    vertices_desconexos.append(v)
-                grafo_desconexo = GrafoBuilder().tipo(self.__grafo.__class__()).vertices(vertices=vertices_desconexos) \
-                    .arestas(completo=True, peso_max=peso_max).build()
+        if paralelas:
+            for i in range(paralelas):
+                par = adjacentes[randint(0, len(adjacentes) - 1)]
+                self._grafo.adiciona_aresta('{}p'.format(i + 1),
+                    par[0], par[1], self._peso(peso_min, peso_max))
 
-                for n, aresta in enumerate(grafo_desconexo.arestas.values()):
-                    self.__grafo.adiciona_aresta(str(n) + 'c',
-                        aresta.v1.rotulo, aresta.v2.rotulo, peso(peso_max))
-                    n += 1
+        if lacos:
+            for i in range(lacos):
+                v = vertices[randint(0, len(vertices) - 1)]
+                self._grafo.adiciona_aresta('{}l'.format(i + 1),
+                v, v, self._peso(peso_min, peso_max))
 
-            if laco:
-                for i in range(qtd_laco):
-                    v = randrange(len(vertices))
-                    self.__grafo.adiciona_aresta(str(i) + 'l',
-                        vertices[v], vertices[v], peso(peso_max))
+        return self
 
-            if qtd > (len(vertices) * (len(vertices) - 1)) / 2:
-                raise GrafoBuilderError('Não é possível gerar um grafo com esta quantidade de arestas')
-            for i in range(qtd):
-                par_v: list
-                if len(vertices) > 2:
-                    par_v = pares_adjacentes.pop(randint(0, len(pares_adjacentes) - 1))
-                else:
-                    par_v = pares_adjacentes.pop()
-                shuffle(par_v)
-                self.__grafo.adiciona_aresta(name_a + str(i),
-                    par_v[0], par_v[1], peso(peso_max))
-                pares_arestas.append(par_v)
+    @arestas.register
+    def _(self, arg: bool, **kwargs):
+        peso_min = kwargs.get('peso_min', 0)
+        peso_max = kwargs.get('peso_max', 1)
+        vertices = [v.rotulo for v in self._grafo.vertices]
+        rotulo_a = 1
+        for v1 in range(len(vertices)):
+            for v2 in range(v1 + 1, len(vertices)):
+                self._grafo.adiciona_aresta('a{}'.format(rotulo_a),
+                    vertices[v1], vertices[v2], self._peso(peso_min, peso_max))
+                rotulo_a += 1
 
-            if paralelas:
-                for i in range(qtd_paralelas):
-                    a = pares_arestas[randint(0, len(pares_arestas) - 1)]
-                    self.__grafo.adiciona_aresta(str(i) + 'p',
-                        a[0], a[1], peso(peso_max))
+        return self
+
+    @arestas.register
+    def _(self, arg:list, **kwargs):
+        for aresta in arg:
+            self._grafo.adiciona_aresta(aresta)
         return self
